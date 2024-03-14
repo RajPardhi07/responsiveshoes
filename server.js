f48dde2058ebe9c3c732c675779561c8b73a4ca8
RajPardhi07 <132877302+RajPardhi07@users.noreply.github.com>
2024-03-14 23:36:29 +0530

    final responsive
    ecomerce

---
+++ server.js
@@ -0,0 +1,51 @@
+import express from 'express'
+import dotenv from "dotenv"
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import authRoutes from "./routes/authRoute.js"
+import categoryRoutes from './routes/categoryRoutes.js'
+import productRoutes from './routes/productRoutes.js'
+import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+//configure env
+dotenv.config();
+
+//database congig
+connectDB();
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const app = express();
+
+//middleware
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes)
+app.use("/api/v1/product", productRoutes)
+
+app.use(express.static(path.join(__dirname, './client/build')))
+
+app.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
+})
+
+app.get("/", (req, res) => {
+     res.send("<h1>Welcome to ecommerce app</h1>")
+});
+
+
+
+//port
+const PORT = process.env.PORT || 8080;
+
+// RUN Listen
+app.listen(PORT, ()=> {
+    console.log(`server running on port ${PORT}`)
+})
